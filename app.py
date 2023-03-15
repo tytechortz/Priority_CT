9cf73a4961ff4067c897998aa253637ded30733b
James Swank <jamesswank@Jamess-Laptop.local>
2023-03-15 16:41:35 -0600

    adds table

--- app.py
+++ app.py
@@ -14,11 +14,45 @@ header = html.Div("Arapahoe Census Tract Data", className="h2 p-2 text-white bg-
 
 template = {"layout": {"paper_bgcolor": bgcolor, "plot_bgcolor": bgcolor}}
 
+gdf = gpd.read_file('ArapahoeCT.shp')
+print(gdf)
+print(gdf.columns)
+
+df_SVI = pd.read_csv('Colorado_SVI.csv')
+
+columnDefs = [
+    {
+        'headerName': 'Census Tract',
+        'field': 'FIPS'
+    },
+]
+
+defaultColDef = {
+    "filter": True,
+    "resizable": True,
+    "sortable": True,
+    "editable": False,
+    "floatingFilter": True,
+    "minWidth": 125
+}
+
+table = dag.AgGrid(
+    id='ct-grid',
+    className="ag-theme-alpine-dark",
+    columnDefs=columnDefs,
+    rowData=df_SVI.to_dict("records"),
+    columnSize="sizeToFit",
+    defaultColDef=defaultColDef,
+    # cellStyle=cellStyle,
+    # dangerously_allow_code=True,
+    dashGridOptions={"undoRedoCellEditing": True, "rowSelection": "single"},
+)
 
 
 app.layout = dbc.Container(
     [
         header,
+        dbc.Row(dbc.Col(table, className="py-4")),
         
     ],
 )
