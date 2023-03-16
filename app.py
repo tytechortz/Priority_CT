2bc734a5dbc440544dcffb86456261d4d561bde6
James Swank <jamesswank@Jamess-Laptop.local>
2023-03-16 09:18:30 -0600

    converts fips to string

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ template = {"layout": {"paper_bgcolor": bgcolor, "plot_bgcolor": bgcolor}}
 
 gdf = gpd.read_file('ArapahoeCT.shp')
 print(gdf)
-print(gdf.columns)
+
 
 df_SVI = pd.read_csv('Colorado_SVI.csv')
 
@@ -48,14 +48,82 @@ table = dag.AgGrid(
     dashGridOptions={"undoRedoCellEditing": True, "rowSelection": "single"},
 )
 
+def blank_fig(height):
+    """
+    Build blank figure with the requested height
+    """
+    return {
+        "data": [],
+        "layout": {
+            "height": height,
+            "template": template,
+            "xaxis": {"visible": False},
+            "yaxis": {"visible": False},
+        },
+    }
+
 
 app.layout = dbc.Container(
     [
         header,
+        dbc.Row(dcc.Graph(id='ct-map', figure=blank_fig(500))),
+        dbc.Row(dcc.RadioItems(
+                id='radio',
+                options=['SVI', 'Income', 'Households'], inline=True,
+            ),
+        ),
         dbc.Row(dbc.Col(table, className="py-4")),
-        
+        dcc.Store(id='map-data', storage_type='session'),
     ],
 )
+
+@app.callback(
+    Output('map-data', 'data'),
+    Input('radio', 'value'),
+)
+def get_data(radio):
+    if radio == 'SVI':
+        df = df_SVI
+    return df.to_json()
+
+@app.callback(
+    Output('ct-map', 'figure'),
+    Input('map-data', 'data')
+)
+def get_figure(selected_data):
+    # sel_dict = selected_row[0]
+    # del sel_dict['Label']
+    # print(sel_dict)
+    df = pd.read_json(selected_data)
+    df['FIPS'] = df["FIPS"].astype(str)
+    print(df.dtypes['FIPS'])
+    # df2 = pd.DataFrame.from_dict(sel_dict, orient='index', columns=['Count'])
+    # df2 = df2.iloc[1: , :]
+    # df2.index.names = ['FIPS']
+    tgdf = gdf.merge(df, on='FIPS')
+    tgdf['Count'] = tgdf['Count'].str.replace(",", "")
+    tgdf.fillna(0,inplace=True)
+    tgdf['Count'] = (tgdf['Count'].astype(int))
+    tgdf = tgdf.set_index('FIPS')
+    # print(tgdf)
+
+    
+
+    fig = px.choropleth_mapbox(tgdf, 
+                                geojson=tgdf.geometry, 
+                                color="Count",                               
+                                locations=tgdf.index, 
+                                # featureidkey="properties.TRACTCE20",
+                                opacity=0.5)
+
+    fig.update_layout(mapbox_style="carto-positron", 
+                      mapbox_zoom=10.4,
+                      mapbox_center={"lat": 39.65, "lon": -104.8},
+                      margin={"r":0,"t":0,"l":0,"b":0},
+                      uirevision='constant')
+
+
+    return fig
 
 if __name__ == "__main__":
     app.run_server(debug=True, port=8080)
